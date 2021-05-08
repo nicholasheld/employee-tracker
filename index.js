d8069ec527e2f5d93696cdac1a03e7c7deb45feb
nicholas <nicholaspheld@gmail.com>
2021-05-08 10:02:30 -0500

    adding prompt questions cases

--- index.js
+++ index.js
@@ -18,110 +18,66 @@ connection.connect(() => {
   runList();
 });
 
-const runList = () => {
+//initial function providing a base menu of options
+function init() {
   inquirer
-.prompt([
-    {
-      type: "list",
-      name: "choice",
-      message: "What would you like to do?",
-      choices: [
-        {
-          name: "View All Employees",
-          value: "VIEW_EMPLOYEES"
-        },
-        {
-          name: "View All Employees By Department",
-          value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
-        },
-        {
-          name: "View All Employees By Manager",
-          value: "VIEW_EMPLOYEES_BY_MANAGER"
-        },
-        {
-          name: "Add Employee",
-          value: "ADD_EMPLOYEE"
-        },
-        {
-          name: "Remove Employee",
-          value: "REMOVE_EMPLOYEE"
-        },
-        {
-          name: "Update Employee Role",
-          value: "UPDATE_EMPLOYEE_ROLE"
-        },
-        {
-          name: "Update Employee Manager",
-          value: "UPDATE_EMPLOYEE_MANAGER"
-        },
-        {
-          name: "View All Roles",
-          value: "VIEW_ROLES"
-        },
-        {
-          name: "Add Role",
-          value: "ADD_ROLE"
-        },
-        {
-          name: "Remove Role",
-          value: "REMOVE_ROLE"
-        },
-        {
-          name: "View All Departments",
-          value: "VIEW_DEPARTMENTS"
-        },
-        {
-          name: "Add Department",
-          value: "ADD_DEPARTMENT"
-        },
-        {
-          name: "Remove Department",
-          value: "REMOVE_DEPARTMENT"
-        },
-        {
-          name: "Quit",
-          value: "QUIT"
-        }
-      ]
-    }
-
-  ]);
-
-
-  // Call the appropriate function depending on what the user chose
- .then((answer)=> {
-
- switch (answer.choice) {
-    case "VIEW_EMPLOYEES":
-      return viewEmployees();
-    case "VIEW_EMPLOYEES_BY_DEPARTMENT":
-      return viewEmployeesByDepartment();
-    case "VIEW_EMPLOYEES_BY_MANAGER":
-      return viewEmployeesByManager();
-    case "ADD_EMPLOYEE":
-      return addEmployee();
-    case "REMOVE_EMPLOYEE":
-      return removeEmployee();
-    case "UPDATE_EMPLOYEE_ROLE":
-      return updateEmployeeRole();
-    case "UPDATE_EMPLOYEE_MANAGER":
-      return updateEmployeeManager();
-    case "VIEW_DEPARTMENTS":
-      return viewDepartments();
-    case "ADD_DEPARTMENT":
-      return addDepartment();
-    case "REMOVE_DEPARTMENT":
-      return removeDepartment();
-    case "VIEW_ROLES":
-      return viewRoles();
-    case "ADD_ROLE":
-      return addRole();
-    case "REMOVE_ROLE":
-      return removeRole();
-    default:
-      return quit();
-  }
-})
-
+      .prompt([
+          {
+              type: 'list',
+              name: 'action',
+              message: 'What would you like to do with the workforce database?',
+              choices: ['Add_department', 'Add_role', 'Add_employee', 'View_departments', 'View_roles', 'View_employees', 'Update_employee_role', 'Update_managers', 'View_employees_by_manager', 'Delete_department', 'Delete_role', 'Delete_employee', 'View_dept_salary_budget', 'Quit']
+          }
+      ])
+      .then((answer) => {
+          switch (answer.action) {
+              case 'Add_department':
+                  addDepartment();
+                  break;
+              case 'Add_role':
+                  addRole();
+                  break;
+              case 'Add_employee':
+                  addEmployee();
+                  break;
+              case 'View_departments':
+                  viewDepartments();
+                  break;
+              case 'View_roles':
+                  viewRoles();
+                  break;
+              case 'View_employees':
+                  viewEmployees();
+                  break;
+              case 'Update_employee_role':
+                  updateEmployeeRole();
+                  break;
+              case 'Update_managers':
+                  updateManagers();
+                  break;
+              case 'View_employees_by_manager':
+                  viewEmployeeByManagers();
+                  break;
+              case 'Delete_department':
+                  deleteDepartment();
+                  break;
+              case 'Delete_role':
+                  deleteRole();
+                  break;
+              case 'Delete_employee':
+                  deleteEmployee();
+                  break;
+              case 'View_dept_salary_budget':
+                  viewDeptSalaryBudget();
+                  break;
+              case 'Quit':
+                  connection.end();
+                  break;
+              default:
+                  console.log('Invalid Choice');
+                  init()
+      }
+  })
+}
 //create functions to call upon
 
