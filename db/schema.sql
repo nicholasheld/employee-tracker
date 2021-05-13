e9c0f76c672d46b235bb1807cecd254c27fe6870
nicholas <nicholaspheld@gmail.com>
2021-05-13 08:18:42 -0500

    added seed data to the end of schema file since the seed file was not working

--- db/schema.sql
+++ db/schema.sql
@@ -25,3 +25,26 @@ CREATE TABLE department (
     name VARCHAR(30),
     PRIMARY KEY (department_id)
 )
+--seeds the database--
+INSERT INTO department
+    (name)
+VALUES
+    ('Engineering'),
+    ('Finance');
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+
+    ('Lead Engineer', 120000, 1),
+    ('Software Engineer', 100000, 1),
+    ('Account Manager', 130000, 2),
+    ('Accountant', 120000, 2);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Michael', 'Jordon', 1, NULL),
+    ('James', 'Bond', 2, 1),
+    ('Jimmy', 'John', 3, NULL),
+    ('Archer', 'Sterling', 4, 2);
